f8d664ed984169e4f4ae32f83e9d681a45bc4334
AleMarra <alemarra@gmail.com>
2012-04-27 11:03:17 -0300

    first commit

---
+++ TestCaseGen/src/TestCaseGen.cpp
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : TestCaseGen.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main() {
+	return 0;
+}
