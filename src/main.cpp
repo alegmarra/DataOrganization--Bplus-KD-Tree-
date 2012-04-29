c18e533069510d01dfb55af86f79e9b1e4ec41d8
Barbazul <terevalis@gmail.com>
2012-04-28 23:55:04 -0300

    No se por que me habia quedado afuera el main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,96 @@
+#include "File/Sequential.h"
+#include "Comparator.h"
+
+struct Record 
+{
+    int linea;
+    int franja_horaria;
+    int falla;
+    int accidente;
+    int formacion;
+};
+
+class IntComparator: public Comparator 
+{
+    public:
+    
+    IntComparator(void * content) : Comparator(content) { };
+    
+    int compareTo(void * target) {
+        int data =* (int*)obj;
+        int targetData =* (int*)target;
+        
+        if (data == targetData) return 0;
+        if (data > targetData) return 1;
+        if (data < targetData) return -1;
+    }
+};
+
+class RecordComparator: public Comparator 
+{
+    public:
+    
+    RecordComparator(void * content) : Comparator(content) { };
+    
+    int compareTo(void * target) {
+        Record * data = (Record*)obj;
+        Record * targetData = (Record*)target;
+        bool result;
+        
+        IntComparator c(&(data->linea));
+        result = c.compareTo(&(targetData->linea));
+        
+        if(result == 0) {
+            c.setData(&(data->franja_horaria));
+            result = c.compareTo(&(targetData->franja_horaria));
+            
+            if(result == 0) {
+                c.setData(&(data->franja_horaria));
+                result = c.compareTo(&(targetData->franja_horaria));
+            }
+        }
+        
+        return result;
+    }
+};
+
+int main() 
+{
+    FileSequential f("/tmp/test.bin", sizeof(Record));
+    f.create();
+ 
+    Record a = {1, 2, 3, 4, 5};
+    Record b = {6, 7, 8, 9, 0};
+    Record c = {1, 5, 3, 4, 2};
+    Record d = {6, 7, 8, 9, 0}; // Este no deberia insertarse porque esta repetido
+
+    RecordComparator comp(&a);    
+    f.insert(&comp, &a);
+    
+    comp.setData(&b);    
+    f.insert(&comp, &b);
+
+    comp.setData(&c);    
+    f.insert(&comp, &c);
+
+    comp.setData(&d);    
+    f.insert(&comp, &d);
+
+    f.reset();
+    
+    void * ptr = f.next();
+    
+    while(ptr != NULL) {
+        Record x =* (Record*)ptr;
+        printf("Linea: %d\n", x.linea);
+        printf("Franja horaria: %d\n", x.franja_horaria);
+        printf("Falla: %d\n", x.falla);
+        printf("Accidente: %d\n", x.accidente);
+        printf("Formacion: %d\n", x.formacion);
+        printf("===================\n");
+        ptr = f.next();
+    }
+    
+    return 0;
+}
+
