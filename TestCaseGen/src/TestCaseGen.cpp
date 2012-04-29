e468b3c96e3aeab41d16340d75f93f5ef2064f03
AleMarra <alemarra@gmail>
2012-04-29 16:53:49 -0300

    Casos de prueba => TestData.txt

--- TestCaseGen/src/TestCaseGen.cpp
+++ TestCaseGen/src/TestCaseGen.cpp
@@ -3,14 +3,109 @@
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Hello World in C++, ANSI-style
 //============================================================================
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
 
-int main() {
 
-	std::cout << "Hello World!" << std::endl;
+std::vector<std::string> recuperarCampos(std::string linea){
+
+	std::vector<std::string> campos;
+	char chr;
+	unsigned indx = 0;
+
+	chr = linea[indx];
+
+	while (indx < linea.size()){
+
+		if ((chr == '[') or (chr ==',')){
+
+			std::string palabra;
+			indx++;
+			chr = linea[indx];
+
+			do{
+			   palabra = palabra + chr;
+		   	   indx++;
+			   chr = linea[indx];
+
+			}while ((chr != ']') and (chr !=',') and (chr != '\000'));
+
+			campos.push_back(palabra);
+		}
+
+		indx++;
+		chr = linea[indx];
+
+	}
+
+    return campos;
+
+}
+
+int main(int argc, char* argv[]) {
+
+	//Cantidad de registros de prueba generados
+	unsigned cantReg;
+
+	if (argc > 0) cantReg = *argv[0];
+	else cantReg = 50;
+
+	std::ifstream inFile("src/fuenteDB.txt");
+	std::ofstream outFile("src/testData.txt");
+
+	std::string linea;
+
+
+	if (inFile.is_open() && outFile.is_open()){
+
+		do{
+			getline (inFile,linea);
+
+			while (!inFile.eof() && (linea != ""))
+			{
+			   std::vector<std::string> campos;
+			   std::vector<std::string>::iterator it;
+
+
+	//		   std::cout << linea << std::endl;
+
+			   campos = recuperarCampos(linea);
+
+			   srand((time(NULL)));
+
+			   unsigned random = (rand()%(campos.size()-1)+1);
+
+			   outFile << campos[0] << ": " << campos[random] << ", ";
+			   getline (inFile,linea);
+			}
+
+			srand ((time(NULL)));
+			outFile << "Formacion: " << (rand() % 2000 +1) << "\n";
+
+			sleep(3);
+
+			cantReg--;
+
+			if (cantReg >0){
+				inFile.clear();
+				inFile.seekg(0, std::ifstream::beg);
+			}
+
+		}while(cantReg>0);
+
+
+		outFile.close();
+		inFile.close();
+	}
+	else{
+		std::cout << "FILES NOT FOUND";
+	}
+
 
 	return 0;
 }
