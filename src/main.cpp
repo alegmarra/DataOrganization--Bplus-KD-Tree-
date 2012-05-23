ee90c2f40bfb475f2ea3116b10c2daaa3624f674
Barbazul <terevalis@gmail.com>
2012-05-23 20:02:16 -0300

    Emprolijado un toque el codigo del main

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,26 @@
 #include "UnitTests/QueryTest.cpp"
 #include "UnitTests/FileBlocksTest.cpp"
 
+#include <iostream>
+
 int main() 
 {
 	Test* test;
 
 	test = new KeyTest();
 	test->run();
+    delete test;
 
 	test = new FileBlocksTest();
 	test->run();
+    delete test;
 
     test = new QueryTest();
     test->run();
-
 	delete test;
+
+    std::cout << std::endl << "UnitTest END" 
+               << std::endl << std::endl;
 
     return 0;
 }
