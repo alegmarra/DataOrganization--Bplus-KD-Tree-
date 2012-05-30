09e8e985686ba945de9887d5505e7b8bd55d7462
Barbazul <terevalis@gmail.com>
2012-05-30 18:46:41 -0300

    Agregado un helper para obtener la key correspondiente de un ID segun nivel

--- src/KDTree/Node/LeafNode.cpp
+++ src/KDTree/Node/LeafNode.cpp
@@ -286,3 +286,4 @@ int LeafNode::deserialize(const char* buffer) {
 
     return bytes;
 }
+
--- src/KDTree/Node/Node.cpp
+++ src/KDTree/Node/Node.cpp
@@ -32,3 +32,8 @@ unsigned Node::getNumElements() {
     return numElements;
 }
 
+
+unsigned getKeyByLevel(ID * id, unsigned level)
+{
+    return id->getKey(level % id->getDimensions());
+}
